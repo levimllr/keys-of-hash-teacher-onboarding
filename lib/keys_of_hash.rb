5dcc43430414b21040277b2064767079c7762ab0
Levi Miller <paullevimiller@gmail.com>
2019-07-17 14:38:47 -0700

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,29 @@
+# require ("pry")
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    key_value_match_array = []
+    arguments.each do |argument|
+      self.each do |key, value|
+        if self[key] == argument
+          key_value_match_array.push(key)
+          # binding.pry
+        end
+      end
+    end
+    # binding.pry
+    return key_value_match_array
   end
 end
+
+# test_hash = {
+#   "sugar glider"=>"Australia",
+#   "aye-aye"=> "Madagascar",
+#   "red-footed tortoise"=>"Panama",
+#   "kangaroo"=> "Australia",
+#   "tomato frog"=>"Madagascar",
+#   "koala"=>"Australia"
+# }
+
+# test_hash.keys_of("Panama")
